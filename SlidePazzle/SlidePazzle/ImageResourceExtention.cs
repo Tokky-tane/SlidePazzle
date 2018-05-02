12f97eb5b47960b20aaefad89f8251804509a834
Tokky_tane <mitokinksa-dos3zero@outlook.jp>
2018-05-02 22:34:36 +0900

    画像の追加

---
+++ SlidePazzle/SlidePazzle/ImageResourceExtention.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace SlidePazzle
+{
+    [ContentProperty ("Source")]
+    class ImageResource : IMarkupExtension
+    {
+        public string Source { get; set; }
+
+        public object ProvideValue(IServiceProvider serviceProovider)
+        {
+            if (Source == null)
+                return null;
+
+            return ImageSource.FromResource(Source);
+        }
+    }
+}
