5aa3bf4bfe5d4daca05647a5109cdb240e70bc9a
Tokky_tane <mitokinksa-dos3zero@outlook.jp>
2018-05-03 09:11:32 +0900

    タップ音を追加

--- SlidePazzle/SlidePazzle/MainPage.xaml.cs
+++ SlidePazzle/SlidePazzle/MainPage.xaml.cs
@@ -4,18 +4,22 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
+using Plugin.SimpleAudioPlayer;
 
 namespace SlidePazzle
 {
 	public partial class MainPage : ContentPage
 	{
-		public MainPage()
+        ISimpleAudioPlayer player = CrossSimpleAudioPlayer.Current;
+        public MainPage()
 		{
 			InitializeComponent();
-		}
+            player.Load("cursor7.mp3");
+        }
 
         void OnPanelTapped(object sender,EventArgs eventArgs)
         {
+            player.Play();
         }
 	}
 }
